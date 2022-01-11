8b59680a45820b4e4c17c377f650508d753025a5
ferz7 <ferz7@LAPTOP-5VVN8KSS>
2022-01-11 02:11:12 +0100

    scrollview finished

--- app/src/main/java/com/android/adevinta/models/Login.kt
+++ app/src/main/java/com/android/adevinta/models/Login.kt
@@ -8,12 +8,6 @@ data class Login(
     val md5: String,
     @SerializedName("password")
     val password: String,
-    @SerializedName("salt")
-    val salt: String,
-    @SerializedName("sha1")
-    val sha1: String,
-    @SerializedName("sha256")
-    val sha256: String,
     @SerializedName("username")
     val username: String,
     @SerializedName("uuid")
